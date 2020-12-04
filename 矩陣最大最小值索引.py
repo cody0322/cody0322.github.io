df9ac906e5ad1db733e2e9452447bdd04c4e817c
cody0322 <65803067+cody0322@users.noreply.github.com>
2020-12-04 22:45:23 +0800

    Delete 矩陣最大最小值索引.py

--- 矩陣最大最小值索引.py
+++
@@ -1,7 +0,0 @@
-a = []
-for i in range(9):
-    a.append(int(input()))
-pmax = a.index(max(a))
-pmin = a.index(min(a))
-print(f"最大值: {max(a)}索引: ({pmax//3}, {pmax%3})")
-print(f"最小值: {min(a)}索引: ({pmin//3}, {pmin%3})")
