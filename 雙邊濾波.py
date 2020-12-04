c36a96f53da005de79008f695db4d25447468cbc
cody0322 <65803067+cody0322@users.noreply.github.com>
2020-12-04 22:32:56 +0800

    Delete 雙邊濾波.py

--- 雙邊濾波.py
+++
@@ -1,8 +0,0 @@
-import cv2
-import matplotlib.pyplot as plt
-img1 = cv2.imread("lena.jpg" , 0)
-img2 = cv2.bilateralFilter(img1,1,100,50)# 1:直徑區域直徑，100:色彩值，50:空間值
-plt.imshow(img1,"gray")
-plt.show()
-plt.imshow(img2 ,"gray")
-plt.show()
