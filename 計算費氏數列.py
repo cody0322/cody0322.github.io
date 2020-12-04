ff608f89de23a30ba11daa779fc35f6ed717a4dd
cody0322 <65803067+cody0322@users.noreply.github.com>
2020-12-04 22:44:34 +0800

    Delete 計算費氏數列.py

--- 計算費氏數列.py
+++
@@ -1,9 +0,0 @@
-def test(num):
-    ans = [0,1,1]
-    for i in range(3,num):
-        x = ans[i-1]+ans[i-2]
-        ans.append(x)
-    for j in ans:
-        print(j,end=" ")
-num = int(input())
-test(num)
